d7d4fa41ea92a410d0e4b437fd94e905a6889811
shraddhapawar-ds <shraddhapaw1112@gmail.com>
2025-08-24 13:26:12 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from langchain.schema import HumanMessage,SystemMessage,AIMessage
-from langchain.chat_models import ChatOpenAI
+from langchain_community.chat_models import ChatOpenAI
 import os
 from dotenv import load_dotenv
 load_dotenv()
